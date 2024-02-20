ced8d29d1a769ab43584daf64de9702a4258dac0
szuckerbrow <146024427+szuckerbrow@users.noreply.github.com>
2024-02-20 15:40:18 -0500

    popups added

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,62 @@
-// crate a map object
-let map = L.map("map", {
-    center: [39.7392, -105],
-    zoom: 4.5
-});
+function createMap(earthquakeLocation) {
 
-// Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    // Adding the tile layer
+    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // baseMaps object to hold the streetmap layer
+    let baseMaps = {
+        "Street Map": streetmap
+    };
+
+    // overlayMaps object to hold the earthquake layer
+    let overlayMaps = {
+        "Earthquake Location": earthquakeLocation
+    };
+
+    // crate a map object
+    let map = L.map("map", {
+        center: [39.7392, -105],
+        zoom: 4.5,
+        layers: [streetmap, earthquakeLocation]
+    });
+
+    // layer control, pass it baseMaps and overlayMaps.
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+
+    var legend = L.control({position: 'bottomright'});
+    
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            depths = [-10, 10, 30, 50, 70, 90],
+            labels = [];
+    
+        // Loop through depth intervals and generate legend items
+        for (var i = 0; i < depths.length; i++) {
+            // Get the color corresponding to the depth
+            var color = getColor(depths[i] + 1);
+    
+            // Create a colored square using CSS and include it in the legend item
+            div.innerHTML +=
+                '<div style="display:inline-block;width:20px;height:20px;background-color:' + color + ';margin-right:5px;"></div>' +
+                depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+        }
+    
+        // Apply CSS styling to create a box around the legend
+        div.style.padding = '10px';
+        div.style.backgroundColor = 'white';
+        div.style.border = '1px solid #ccc';
+    
+        return div;
+    };
+    
+    
+    legend.addTo(map);
+
+};
 
 // url for all earthquake data from past 7 days
 let earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -43,6 +92,8 @@ function getColor(depth) {
 // create function to place cirlces on map for earthquakes
 function plotEarthquakes(data) {
     let earthquakeFeatures = data.features;
+    // initialize array to hold earthquake markers
+    let markers = [];
 
     // Loop through each feature to access its geometry
     earthquakeFeatures.forEach(function(feature) {
@@ -58,46 +109,34 @@ function plotEarthquakes(data) {
         let magnitude = feature.properties.mag;
         let radius = Math.pow(magnitude, 2) * 3000;
         // let radius = Math.pow(10, magnitude) * 3;
+        // let radius = magnitude * 3;
      
         let color = getColor(depth);
 
         // Create a circle at the earthquake's coordinates with radius
-        L.circle([lat, lng], {
+        let marker = L.circle([lat, lng], {
             radius: radius,
-            color: color,
+            color: "Black",
+            weight: 0.5,
             fillColor: color,
             fillOpacity: 0.5
-        }).addTo(map);
+        });
+        
+        // Add popup to the marker
+        marker.bindPopup(`<h3>Magnitude: ${magnitude}</h3><h3>Location: ${feature.properties.place}</h3><h3>Depth: ${depth} km</h3>`);
+        
+        markers.push(marker);
+
+        // L.circleMarker([lat, lng], {
+        //     fillOpacity: 1,
+        //     weight: 0.5,
+        //     color: "Black",
+        //     fillColor: color,
+        //     radius: radius
+        // }).addTo(map);
     });
 
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info legend'),
-        depths = [-10, 10, 30, 50, 70, 90],
-        labels = [];
-
-    // Loop through depth intervals and generate legend items
-    for (var i = 0; i < depths.length; i++) {
-        // Get the color corresponding to the depth
-        var color = getColor(depths[i] + 1);
-
-        // Create a colored square using CSS and include it in the legend item
-        div.innerHTML +=
-            '<div style="display:inline-block;width:20px;height:20px;background-color:' + color + ';margin-right:5px;"></div>' +
-            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
-    }
-
-    // Apply CSS styling to create a box around the legend
-    div.style.padding = '10px';
-    div.style.backgroundColor = 'white';
-    div.style.border = '1px solid #ccc';
-
-    return div;
-};
-
-
-legend.addTo(map);
+    createMap(L.layerGroup(markers));
 
 
 };
